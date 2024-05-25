e8502812e43f725bb97b01f16319d6313981d9b2
fiyarraARP19 <126551721+fiyarraARP19@users.noreply.github.com>
2024-05-25 20:06:54 +0700

    Initial commit

---
+++ database/migrations/2024_05_25_125515_pasiens.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pasien');
+            $table->integer('umur');
+            $table->date('tgl_lahir');
+            $table->string('nik');
+            $table->string('keluhan');
+            $table->date('tgl_berobat');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pasiens', function (Blueprint $table) {
+            //
+        });
+    }
+};
